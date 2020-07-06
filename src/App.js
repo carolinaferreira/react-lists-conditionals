d1fced6765e926db6f440a04872026670d2f50f5
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-06 11:06:50 -0300

    added a method to display each character from text, delete method and stylesheet to present the characters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
@@ -11,7 +12,21 @@ class App extends Component {
     this.setState({ userInput: event.target.value})
   }
 
+  deleteCharHandler = (index) => {
+    const  input =  this.state.userInput.split('');
+    input.splice(index, 1);
+    const updatedInput = input.join('');
+    this.setState({userInput: updatedInput});
+  };
+
   render() {
+    const charList = this.state.userInput.split('').map( (char, index) => {
+      return <Char 
+        character={char} 
+        key={index}
+        clicked={()=> this.deleteCharHandler(index)}/>
+    });
+
     return (
       <div className="App">
         <input 
@@ -20,25 +35,7 @@ class App extends Component {
           value={this.state.userInput}></input>
         <p>{this.state.userInput.length}</p>
         <Validation inputLenght={this.state.userInput.length}/>
-        <ol>
-          <li>Create an input field (in App component) with a change listener 
-              which outputs the length of the entered text below it (e.g. in a 
-              paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the
-              text length as a prop</li>
-          <li>Inside the ValidationComponent, either output "Text too short" or
-              "Text long enough" depending on the text length (e.g. take 5 as a
-              minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an
-              inline box (=> display: inline-block, padding: 16px, text-align:
-              center, margin: 16px, border: 1px solid black).</li>
-          <li>Render a list of CharComponents where each CharComponent receives
-              a different letter of the entered text (in the initial input 
-              field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the
-              entered text.</li>
-        </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+        {charList}
       </div>
     );
   }
